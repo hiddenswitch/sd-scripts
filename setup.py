0a1e7faba610be2ca2a4ad941c26c8b0a9dc6fda
Jeff Ding <@gmail.com>
2023-11-14 10:30:05 -0800

    Updated setup.py to include med_config file

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name="sd_scripts",
       install_requires=[open("requirements.txt").readlines()],
-      packages=find_packages())
+      packages=find_packages(),
+      package_data={
+            'blip': ['med_config.json'],
+      },
+)
